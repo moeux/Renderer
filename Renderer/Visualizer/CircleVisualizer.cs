493ff43e0acc3474e69f6dfc517d4beba6366b74
moeux <68925026+moeux@users.noreply.github.com>
2026-02-28 22:37:26 +0100

    refactor: Simplify CircleVisualizer radius update logic for better readability

--- Renderer/Visualizer/CircleVisualizer.cs
+++ Renderer/Visualizer/CircleVisualizer.cs
@@ -10,18 +10,20 @@ public class CircleVisualizer() : VisualizerBase("Circle")
     private const int Speed = 300;
     private SKPaint _paint = new() { Color = SKColors.Red, IsAntialias = true };
     private int _radius = 50;
+    private int _radiusStep = 1;
     private double _x;
 
     protected override void DrawFrame(SKCanvas canvas, Rect bounds, long elapsedTicks)
     {
         var elapsedTime = (double)elapsedTicks / TimeSpan.TicksPerSecond;
         _x += Speed * elapsedTime;
-        _radius += _radius switch
+        _radiusStep = _radius switch
         {
             <= 10 => 1,
             >= 50 => -1,
-            _ => 1
+            _ => _radiusStep
         };
+        _radius += _radiusStep;
 
         if (_x + _radius >= bounds.Right)
             canvas.DrawCircle(
